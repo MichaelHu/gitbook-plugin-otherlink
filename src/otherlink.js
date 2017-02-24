f7c5d6cbd44d5c110b276a20162c0411949c7dd7
hudamin <hdm258i@gmail.com>
2017-02-24 20:37:18 +0800

    v1.0.0: take bookRoot into consideration

--- src/otherlink.js
+++ src/otherlink.js
@@ -9,20 +9,31 @@ require( [ 'gitbook', 'jquery' ], function( gitbook, $ ) {
     }
 
     function fixLinks( $links ) {
+        var bookRoot = gitbook.state.bookRoot;
+        if ( !/\/$/.test( bookRoot ) ) {
+            bookRoot += '/';
+        } 
         $links.each( function( index, link ) {
             var $link = $( link ), href = $link.attr( 'href' );
             if ( /^ref:\/\//.test( href ) ) {
-                $link.attr( 'href', href.replace( /^ref:\/\//, '' ) );
+                $link.attr( 
+                    'href'
+                    , bookRoot + href.replace( /^ref:\/\//, '' ) 
+                );
             }
         } );
     }
 
     // invocation on pageready 
-    fixSummaryLinks();
-    fixPageLinks();
-    
+    gitbook.push( function() {
+        // gitbook.state is now ready
+        fixSummaryLinks();
+        fixPageLinks();
+    } );
+
     // page.change invocation
     gitbook.events.on( 'page.change', function() {
+        // console.log( gitbook );
         fixSummaryLinks();
         fixPageLinks();
     } );
